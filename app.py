bebac8967024c5d6910a39de08fa15df1f586656
danielveloso20 <160804004+danielveloso20@users.noreply.github.com>
2024-03-20 22:55:38 +0000

    teste

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Mensagem Custom v1.0"
+    return "Hello World"
